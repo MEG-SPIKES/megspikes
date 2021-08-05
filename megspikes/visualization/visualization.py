3974ead9e751ff07f4ab5e3a18b059ef4ba4a85d
vagechirkov <vagechirkov@gmail.com>
2021-08-05 17:46:49 +0200

    fix plot

--- megspikes/visualization/visualization.py
+++ megspikes/visualization/visualization.py
@@ -5,7 +5,8 @@ import xarray as xr
 from scipy import signal
 import holoviews as hv
 # from holoviews import opts
-import hvplot.xarray
+# import hvplot.xarray
+from sklearn import preprocessing
 import panel.widgets as pnw
 import panel as pn
 hv.extension('bokeh')
@@ -64,8 +65,33 @@ class PlotPipeline():
         return fig
 
     def plot_sources_and_detections(self, ds: xr.Dataset,
-                                    sel_pipe: str = 'aspire_alphacsc_run_3',
+                                    sel_pipe: str = 'aspire_alphacsc_run_1',
                                     filter_ica: bool = True):
+        def select_detections(ts_for_overlay, sel_pipe,
+                              detection_property='detection',
+                              by='ica_component',
+                              name='ica_peak_detection'):
+
+            detections = xr.zeros_like(ts_for_overlay)
+            detections.name = name
+
+            sel_ica_component = dict(detection_property=by,
+                                     pipeline=sel_pipe)
+            ica_source_ind = ds.detection_properties.loc[sel_ica_component]
+            for sens in ds.sensors.values:
+                for ica_comp_ind in ds.ica_component.values:
+                    sel_detections = dict(
+                        detection_property=detection_property,
+                        pipeline=sel_pipe,
+                        sensors=sens)
+                    detections.loc[sens, ica_comp_ind] = (
+                        ts_for_overlay.loc[sens, ica_comp_ind] *
+                        ds.detection_properties.loc[sel_detections])
+                    mask = ica_source_ind.loc[sens] != ica_comp_ind
+                    detections.loc[sens, ica_comp_ind][mask] *= 0
+
+            detections = detections.where(detections != 0)
+            return detections
 
         select_sensors = pn.widgets.Select(
             name='Sensors', options=['grad', 'mag'])
@@ -84,54 +110,74 @@ class PlotPipeline():
             sfreq = 200
             freq = np.array([20, 90]) / (sfreq / 2.0)
             b, a = signal.butter(3, freq, "pass")
-            ica_src2.values = signal.filtfilt(b, a, ica_src2.values)
+            for i in range(ica_src2.values.shape[0]):
+                ica_src2.values[i, :] = signal.filtfilt(
+                    b, a, ica_src2.values[i, :])
+                ica_src2.values[i, :] = preprocessing.robust_scale(
+                    ica_src2.values[i, :])
 
-        sel_detections = dict(detection_property='detection',
-                              pipeline=sel_pipe)
-        detections = ica_src * ds.detection_properties.loc[sel_detections]
-        detections.name = 'ica_peak_detection'
+        detections = select_detections(
+            ica_src, sel_pipe, name='ica_peak_detection')
 
-        sel_ica_component = dict(detection_property='ica_component',
-                                 pipeline=sel_pipe)
-        ica_source_ind = ds.detection_properties.loc[sel_ica_component]    
-        for sens in ds.sensors.values:
-            for ica_comp_ind in ds.ica_component.values:
-                mask = ica_source_ind.loc[sens] != ica_comp_ind
-                detections.loc[sens, ica_comp_ind][mask] *= 0
-        detections = detections.where(detections != 0)
+        detections_filt = select_detections(
+            ica_src2, sel_pipe, name='ica_peak_detection_filt')
 
-        subcorrs = ds.detection_properties.loc[
-            dict(detection_property='detection',
-                 pipeline=sel_pipe)].copy(deep=True)
-        subcorrs.name = 'ica_peaks_subcorrs'
+        detections_cleaned = select_detections(
+            ica_src, sel_pipe, detection_property='selected_for_alphacsc',
+            name='selected_for_alphacsc')
 
-        ds_plot = xr.merge([ica_src, ica_src2, detections, subcorrs])
+        detections_cleaned_filt = select_detections(
+            ica_src2, sel_pipe, detection_property='selected_for_alphacsc',
+            name='selected_for_alphacsc_filt')
+
+        ds_plot = xr.merge([
+            ica_src, ica_src2, detections, detections_filt,
+            ds.detection_properties.sel(pipeline=sel_pipe),
+            detections_cleaned, detections_cleaned_filt])
         ds_plot = ds_plot.interactive().sel(time=time_slider).sel(
             sensors=select_sensors)
 
         plot_src = (
             ds_plot.original_ica_sources
             .hvplot(kind='line', width=1000, height=100,
-                    yaxis=None, xaxis=None, subplots=True,
-                    by=['ica_component']).cols(1))
+                    yaxis=None, xaxis=None, subplots=True, color='k',
+                    line_width=0.5, by=['ica_component']).cols(1))
 
         plot_src_filt = (
             ds_plot.filtered_ica_sources
             .hvplot(kind='line', width=1000, height=100,
-                    yaxis=None, xaxis=None, subplots=True,
-                    by=['ica_component']).cols(1))
+                    yaxis=None, xaxis=None, subplots=True, color='k',
+                    line_width=0.5, by=['ica_component']).cols(1))
+
+        kw_params = dict(width=1000, height=100,
+                         tools=['hover', 'lasso_select', 'tap', 'box_select'],
+                         yaxis=None, xaxis=None, subplots=True, kind='scatter',
+                         by=['ica_component'])
 
         plot_det = (
             ds_plot.ica_peak_detection
-            .hvplot(kind='scatter', color='r', width=1000, height=100,
-                    yaxis=None, xaxis=None, subplots=True,
-                    by=['ica_component']).cols(1))
+            .hvplot(color='b', alpha=0.2, **kw_params).cols(1))
+
+        plot_det_filt = (
+            ds_plot.ica_peak_detection_filt
+            .hvplot(color='b', alpha=0.2, **kw_params).cols(1))
+
+        plot_selected_det = (
+            ds_plot.selected_for_alphacsc
+            .hvplot(color='r', alpha=0.5, **kw_params).cols(1))
+
+        plot_selected_det_filt = (
+            ds_plot.selected_for_alphacsc_filt
+            .hvplot(color='r', alpha=0.5, **kw_params).cols(1))
 
         table = (
-            ds_plot.ica_peaks_subcorrs
+            ds_plot.detection_properties
             .hvplot(kind='table', y='detection_property', x='time', width=1000,
                     by=['ica_component']))
-        return (plot_src*plot_det + plot_src_filt + table).cols(1)
+        ts_det = plot_src*plot_det*plot_selected_det
+        ts_det_filt = plot_src_filt*plot_det_filt*plot_selected_det_filt
+        full_plot = (ts_det + ts_det_filt + table).cols(1)
+        return full_plot
 
     def plot_ica_peaks_localizations(self):
         pass
