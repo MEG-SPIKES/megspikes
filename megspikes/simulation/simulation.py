29fb85b37c8c66e34c1a3901278a609c28310a05
vagechirkov <vagechirkov@gmail.com>
2021-08-17 13:41:35 +0200

    Update simulation and simulation tests

--- megspikes/simulation/simulation.py
+++ megspikes/simulation/simulation.py
@@ -1,7 +1,7 @@
 import os.path as op
 import shutil
 from pathlib import Path
-from typing import Union
+from typing import List, Union, Tuple
 
 import mne
 import numpy as np
@@ -20,91 +20,122 @@ mne.set_log_level("ERROR")
 class Simulation:
 
     def __init__(self, root_dir_path: Union[Path, str] = None,
-                 case: Union[str, None] = None):
+                 time_between_events: int = 1, atlas: str = 'aparc.a2009s'):
         if isinstance(root_dir_path, (str, Path)):
             if Path(root_dir_path).is_dir():
                 self.root = Path(root_dir_path)
         else:
             self.root = Path(op.dirname(__file__))
+        self.time_between_events = time_between_events
+        # SEE: https://europepmc.org/article/PMC/2937159
+        self.atlas = atlas
 
-        if isinstance(case, str):
-            self.case = case
-        else:
-            self.case = 'sample'
+        # path to the data reqired for the simulation
         self.spikes_file = Path(op.dirname(__file__)) / 'data' / 'spikes.npy'
+        assert self.spikes_file.is_file(), (
+            "No spikes.npy in the simulation data folder:"
+            f" {self.spikes_file.parent}")
         self.case_info = Path(op.dirname(__file__)) / 'data' / 'case_info.xlsx'
+        assert self.case_info.is_file(), (
+            "No case_info.xlsx in the simulation data folder:"
+            f" {self.case_info.parent}")
 
-    def simulate_dataset(self, length: int = 600,
-                         spikes: Union[str, None] = None,
-                         n_sources: int = 1):
-        # length seconds
-        # TODO: add spikes reading option
-        if not isinstance(spikes, str):
-            self.spike_shapes = np.load(self.spikes_file)
-            # peaks in self.spike_shapes
-            self.peak_times = [0.195, 0.110, 0.290, 0.260]
+        # Load spikes
+        self.spike_shapes = np.load(self.spikes_file)
 
-        self.load_mne_dataset()
-        self._simulate_events(length, n_sources)
-        self._simulate_raw()
-        self._add_annotation()
-        self.simulate_data_structure()
-        self.simulate_case()
-
-    def load_mne_dataset(self):
-        data_path = sample.data_path()
-        self.subjects_dir = op.join(data_path, 'subjects')
-        self.mne_subject = 'sample'
-        meg_path = op.join(data_path, 'MEG', self.mne_subject)
-
-        # fif info
-        fname_info = op.join(meg_path, 'sample_audvis_raw.fif')
-        info = mne.io.read_info(fname_info, verbose=False)
-        meg_channels = mne.pick_types(info, meg=True, exclude=[])
-        self.info = mne.pick_info(info, meg_channels)
-        self.info['bads'] = []
-        self.info['sfreq'] = 1000  # Hz
-        self.tstep = 1 / self.info['sfreq']
-
-        # forward solution of the sample subject.
-        fwd_fname = op.join(meg_path, 'sample_audvis-meg-oct-6-fwd.fif')
-        self.fwd = mne.read_forward_solution(fwd_fname, verbose='error')
-        self.fwd['info']['bads'] = []
-        self.src = self.fwd['src']
-
-        # raw
-        raw_path = op.join(meg_path, 'sample_audvis_raw.fif')
-        self.raw = mne.io.read_raw_fif(raw_path)
-
-        self.meg_path = meg_path
-
-    def _simulate_events(self, length=15, n_sources=1):
-        self.n_events = length // n_sources
-        self.n_sources = n_sources
-
-        n_events = self.n_events
-        n_sources = self.n_sources
-        events = np.zeros((n_events * n_sources, 3))
-        events[:, 0] = 1000 * np.arange(n_events * n_sources)
-        self.spikes = events[:, 0]
-        events[:, 2] = sum([[n+1]*n_events for n in range(n_sources)], [])
-        event_id = {
+        # Events id
+        self.event_id = {
             'spike_shape_1': 1,
             'spike_shape_2': 2,
             'spike_shape_3': 3,
             'spike_shape_4': 4}
 
         # label, activation (nAm)
-        activations = {
+        self.activations = {
             'spike_shape_1': [('G_temp_sup-G_T_transv-rh', 120)],
+            'spike_shape_2': [('S_subparietal-rh', 280)],
+            'spike_shape_3': [('S_subparietal-lh', 120)],
+            'spike_shape_4': [('S_subparietal-lh', 120)]
         }
-        # 'spike_shape_2': [('S_subparietal-rh', 280)],
-        # 'spike_shape_3': [('S_subparietal-lh', 120)],
-        # 'spike_shape_4': [('S_subparietal-lh', 120)],
 
-        # SEE: https://europepmc.org/article/PMC/2937159
-        annot = 'aparc.a2009s'
+        # Set approximate peak times for each spike in self.spike_shapes
+        self.peak_times = [0.195, 0.110, 0.290, 0.260]
 
+    def simulate_dataset(self, n_events: List[int] = [15, 0, 0, 0],
+                         simultaneous: List[bool] = [False]*4,
+                         sfreq: float = 1000., snr: float = 1.):
+        # length seconds
+        # TODO: add spikes reading option
+        info, fwd, raw = self._read_mne_sample_dataset()
+        events = self._prepare_events(n_events, simultaneous, sfreq=sfreq)
+        source_simulator = self._simulate_sources(events, fwd['src'], 1/sfreq)
+        simulation = self._simulate_raw(source_simulator, fwd, info, raw, snr)
+        self._add_annotation(simulation, events, sfreq)
+        self._simulate_data_structure(simulation)
+        self._simulate_case()
+        self.events = events
+        self.raw_simulation = simulation
+
+    def _read_mne_sample_dataset(self) -> Tuple[mne.Info, mne.Forward,
+                                                mne.io.Raw]:
+        """Load sample MNE dataset.
+
+        Notes
+        -----
+        MNE-Python dataset description:
+        https://mne.tools/stable/overview/datasets_index.html#sample
+        """
+        data_path = sample.data_path()
+        self.subjects_dir = op.join(data_path, 'subjects')
+        self.mne_subject = 'sample'
+        meg_path = op.join(data_path, 'MEG', self.mne_subject)
+        self.meg_path = meg_path
+
+        # fif info
+        fname_info = op.join(meg_path, 'sample_audvis_raw.fif')
+        info = mne.io.read_info(fname_info)
+        meg_channels = mne.pick_types(info, meg=True, exclude=[])
+        info = mne.pick_info(info, meg_channels)
+        info['bads'] = []
+        info['sfreq'] = 1000  # Hz
+        # self.tstep = 1 / info['sfreq']
+
+        # forward solution
+        fwd_fname = op.join(meg_path, 'sample_audvis-meg-oct-6-fwd.fif')
+        fwd = mne.read_forward_solution(fwd_fname)
+        fwd['info']['bads'] = []
+        # self.src = self.fwd['src']
+
+        # fif raw
+        raw_path = op.join(meg_path, 'sample_audvis_raw.fif')
+        raw = mne.io.read_raw_fif(raw_path)
+        return info, fwd, raw
+
+    def _prepare_events(self, n_events: List[int] = [15, 0, 0, 0],
+                        simultaneous: List[bool] = [False]*4,
+                        sfreq: float = 1000) -> np.ndarray:
+        all_sources_events = []
+        first_time = 0
+        t_steps = self.time_between_events * sfreq
+        for s, n in enumerate(n_events):
+            tmp_events = np.zeros((n_events[s], 3))
+            last_time = first_time + n
+            # events timepoints
+            tmp_events[:, 0] = t_steps * np.arange(first_time, last_time)
+            # events id
+            tmp_events[:, 2] = np.repeat(s+1, n)
+            all_sources_events.append(tmp_events)
+
+            if not simultaneous[s]:
+                first_time += n
+
+        events = np.vstack(all_sources_events)
+        return events
+
+    def _simulate_sources(self, events: np.ndarray, src: mne.SourceSpaces,
+                          tstep: float, scaler: float = 1e-10
+                          ) -> mne.simulation.SourceSimulator:
+        activations = self.activations
         # Load the necessary label names.
         label_names = sorted(
             set(activation[0] for activation_list in activations.values()
@@ -112,65 +143,58 @@ class Simulation:
         region_names = list(activations.keys())
         self.labels = []
 
-        self.source_simulator = mne.simulation.SourceSimulator(
-            self.src, tstep=self.tstep)
+        source_simulator = mne.simulation.SourceSimulator(
+            src, tstep=tstep)
 
-        for region_id, region_name in enumerate(region_names, 1):
-            event_inx = region_id
-            if region_id == 2:
-                # To make events 1 and 2 simultaneous
-                event_inx = 1
+        for region_name in region_names:
+            event_inx = self.event_id[region_name]
             events_tmp = events[np.where(events[:, 2] == event_inx)[0], :]
-            # print(events_tmp.shape, event_inx, events_tmp[:, 0].max())
-            i = 0
-            label_name = activations[region_name][i][0]
+            label_name = activations[region_name][0][0]
             label_tmp = mne.read_labels_from_annot(
-                self.mne_subject, annot, subjects_dir=self.subjects_dir,
-                regexp=label_name, verbose=False)
-            label_tmp = label_tmp[0]
+                self.mne_subject, self.atlas, subjects_dir=self.subjects_dir,
+                regexp=label_name)[0]
             self.labels.append(label_tmp)
-            amplitude_tmp = activations[region_name][i][1]
-            wf_tmp = 1e-10 * self.spike_shapes[region_id-1]
-            # print(wf_tmp.shape)
-            self.source_simulator.add_data(
+            amplitude_tmp = activations[region_name][0][1]
+            wf_tmp = scaler * self.spike_shapes[event_inx - 1]
+            source_simulator.add_data(
                 label_tmp, amplitude_tmp * wf_tmp, np.int32(events_tmp))
-
         self.label_names = label_names
-        self.events = events
-        self.annot = annot
-        self.event_id = event_id
-        self.activations = activations
+        return source_simulator
 
-    def _add_annotation(self):
-        spikes = np.float64(self.spikes)
-        all_peaks = self.peak_times[:self.n_sources]
-        spikes += np.array(
-            [[i]*self.n_events for i in all_peaks]).flatten()
-        spikes_annot = mne.Annotations(
-            onset=spikes,  # in seconds
-            duration=[0.001]*len(spikes),  # in seconds, too
-            description=sum(
-                [[f'SP{i+1}']*self.n_events for i in range(self.n_sources)],
-                []))
-        self.raw_simulation.set_annotations(spikes_annot)
-
-    def _simulate_raw(self):
+    def _simulate_raw(self, source_simulator: mne.simulation.SourceSimulator,
+                      fwd: mne.Forward, info: mne.Info, raw: mne.io.Raw,
+                      snr: float = 1.) -> mne.io.Raw:
         iir_filter = mne.time_frequency.fit_iir_model_raw(
-            self.raw, order=5, picks='meg', tmin=60, tmax=180)[1]
-
+            raw, order=5, picks='meg', tmin=60, tmax=180)[1]
         rng = np.random.RandomState(0)
-        self.raw_simulation = mne.simulation.simulate_raw(
-            self.info, self.source_simulator, forward=self.fwd)
-        snr = 20.
-        noise_cov = mne.make_ad_hoc_cov(self.raw_simulation.info)
+        simulation = mne.simulation.simulate_raw(
+            info, source_simulator, forward=fwd)
+        noise_cov = mne.make_ad_hoc_cov(simulation.info)
         # Scale the noise to achieve the desired SNR
-        noise_cov['data'] *= (20. / snr) ** 2
-        mne.simulation.add_noise(self.raw_simulation, cov=noise_cov,
+        noise_cov['data'] *= snr
+        mne.simulation.add_noise(simulation, cov=noise_cov,
                                  iir_filter=iir_filter, random_state=rng)
+        return simulation
 
-    def simulate_data_structure(self):
+    def _add_annotation(self, simulation: mne.io.Raw, events: np.ndarray,
+                        sfreq: float = 1000.):
+        times = events[:, 0] / sfreq
+        sources = np.unique(events[:, 2])
+        labels = [f"SRC{int(i)}" for i in events[:, 2]]
+        for s in sources:
+            times[events[:, 2] == s] += self.peak_times[int(s)-1]
+
+        spikes_annot = mne.Annotations(
+            onset=times,  # in seconds
+            duration=[0.001]*len(times),  # in seconds
+            description=labels)
+        return simulation.set_annotations(spikes_annot)
+
+    def _simulate_data_structure(self, simulation: mne.io.Raw):
         shutil.copy(str(self.case_info), str(self.root))
-        case_dir = self.root / self.case
+
+        # Create folder
+        case_dir = self.root / self.mne_subject
         fraw = case_dir / 'MEG_data' / 'tsss_mc' / 'sample_raw_tsss_mc.fif'
         fraw2 = (case_dir / 'MEG_data' / 'tsss_mc_artefact_correction'
                  / 'sample_raw_tsss_mc_art_corr.fif')
@@ -178,23 +202,26 @@ class Simulation:
         Path.mkdir(fraw2.parent, exist_ok=True, parents=True)
         ernoise = case_dir / 'MEG_data' / 'empty_room' / 'ernoise-cov.fif'
         Path.mkdir(ernoise.parent, exist_ok=True, parents=True)
+        Path.mkdir(case_dir / 'forward_model', exist_ok=True, parents=True)
 
-        if hasattr(self, 'raw_simulation'):
-            self.raw_simulation.save(str(fraw2), overwrite=True)
-        else:
+        # Save simulation
+        simulation.save(str(fraw2), overwrite=True)
+
+        if False:
             data, _ = simulate_raw_fast(seconds=1, sampling_freq=1000)
             data.save(str(fraw2), overwrite=True)
             self.raw_simulation = data
-        Path.mkdir(case_dir / 'forward_model', exist_ok=True, parents=True)
 
+        # Copy .trans file
         shutil.copy(
             op.join(self.meg_path, 'sample_audvis_raw-trans.fif'),
             str(case_dir / 'forward_model' / 'checked_visually_trans.fif'))
         shutil.copy(op.join(self.meg_path, 'ernoise-cov.fif'), str(ernoise))
 
-    def simulate_case(self):
+    def _simulate_case(self):
         case = CaseManager(
-            root=self.root, case='sample', free_surfer=self.subjects_dir)
+            root=self.root, case=self.mne_subject,
+            free_surfer=self.subjects_dir)
         case.set_basic_folders()
         case.select_fif_file(case.run)
         case.prepare_forward_model()
@@ -208,7 +235,7 @@ class Simulation:
         nb.save(t1, self.fresection.with_name("T1.nii"))
 
         # Save resection
-        self.save_resection_as_nifti(self.fresection)
+        # self.save_resection_as_nifti(self.fresection)
 
     def save_resection_as_nifti(self, fsave: Union[str, Path]):
         # TODO: add more labels
